4fea6b54c68793860e75fa78d68ed14ffab87851
William <william.v.wang@gmail.com>
2018-09-12 20:44:41 -0500

    Parsing fixes and code reorganization

--- app.py
+++ app.py
@@ -3,27 +3,7 @@ import difflib
 from dateutil.parser import parse
 from datetime import *
 from flask import Flask, jsonify, render_template, request
-
-teamIds = {}
-resp = requests.get('http://www.espn.com/college-football/teams').text
-start = 0
-try:
-    while True:
-        start = resp.index('www.espn.com/college-football/team/_/id', start)
-        idEnd = resp.index('/', start + 40)
-        teamId = resp[start+40:idEnd]
-        nameStart = resp.index('>', start) + 1
-        nameEnd = resp.index('<', nameStart)
-        teamName = resp[nameStart:nameEnd]
-        teamIds[teamName.lower()] = (teamId, teamName)
-        start += 1
-except:
-    pass
-
-teamIds['texas'] = teamIds['texas-longhorns']
-teamIds['ut austin'] = teamIds['texas-longhorns']
-teamIds['ut'] = teamIds['texas-longhorns']
-teamIds['longhorn'] = teamIds['texas-longhorns']
+from . import TeamsByName, fuzzymatch
 
 app = Flask(__name__)
 
@@ -40,8 +20,8 @@ def getDate(body):
 
 @app.route('/<teamname>')
 def getTeamStats(teamname):
-    teamname = difflib.get_close_matches(teamname.lower(), teamIds.keys(), 1, 0.2)[0]
-    team_id, teamname = teamIds[teamname]
+    teamname = fuzzymatch(teamname.lower(), 1)[0]
+    team_id, teamname = TeamsByName[teamname]
     for year in range(datetime.now().year, 2001, -1):
         resp = requests.get('http://www.espn.com/college-football/team/schedule/_/id/' + team_id + '/year/' + str(year)).text
         if 'greenfont' in resp:
@@ -50,11 +30,10 @@ def getTeamStats(teamname):
             today = datetime.now()
             return render_template('results.html', count=(today - lastWin).days, school=teamname, school_id=team_id)
 
-
 @app.route('/autocomplete')
 def autocomplete():
     text = request.args.get('text')
-    data = [{"id": teamIds[name][0], "name": teamIds[name][1]} for name in difflib.get_close_matches(text.lower(), teamIds.keys(), 3, 0.2)]
+    data = [{"id": TeamsByName[name][0], "name": TeamsByName[name][1]} for name in fuzzymatch(text, 3)]
     return jsonify(data)
 
 if __name__ == "__main__":
