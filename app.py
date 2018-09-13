e1a6e425a36cdf0b34a71760b8159737ce7ab6fe
Raymond Chee <rjchee@gmail.com>
2018-09-13 02:01:56 -0400

    Move __init__.py to utils

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import requests
 from dateutil.parser import parse
 from datetime import *
 from flask import Flask, jsonify, render_template, request
-from __init__ import TeamsByName, fuzzymatch
+from utils import TeamsByName, fuzzymatch
 
 app = Flask(__name__)
 
