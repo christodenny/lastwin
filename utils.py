ae13949a7929ceafc64a1fa4b3ee3000a81cc673
christodenny <chris.denny@utexas.edu>
2019-11-08 00:39:07 -0800

    Add support for nfl teams

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import requests
 import difflib
 import html
 
-def getTeams():
+def getCfbTeams():
     teamMap = {}
     try:
         resp = requests.get('http://www.espn.com/college-football/teams', timeout=15).text
@@ -23,7 +23,39 @@ def getTeams():
                 teamName = html.unescape(resp[nameStart:nameEnd])
                 # print(teamName)
                 # ex: teamMap['texas longhorns'] = (251, 'Texas Longhorns')
-                teamMap[teamName.lower()] = (teamId, teamName)
+                teamMap[teamName.lower()] = (teamId, teamName, "cfb")
+            else:
+                teamSet.add(teamId)
+            cursor += 1
+    except ValueError:
+        print('{} teams captured'.format(len(teamMap)))
+        return teamMap
+    except:
+        print('getTeams: unexpected error.')
+        raise
+
+def getNflTeams():
+    teamMap = {}
+    try:
+        resp = requests.get('http://www.espn.com/nfl/teams', timeout=15).text
+        searchString = '/nfl/team/_/name/'
+        skipLength = len(searchString)
+        teamSet = set()
+        cursor = 0
+        while True:
+            cursor = resp.index(searchString, cursor)
+            idEnd = resp.index('/', cursor + skipLength)
+            teamId = resp[cursor+skipLength : idEnd]
+            print(teamId)
+            if teamId in teamSet:
+                # skip over <a> ending angle bracket
+                cursor = resp.index('<h2', cursor)
+                nameStart = resp.index('>', cursor) + 1
+                nameEnd = resp.index('<', nameStart)
+                teamName = html.unescape(resp[nameStart:nameEnd])
+                print(teamName)
+                # ex: teamMap['texas longhorns'] = (251, 'Texas Longhorns')
+                teamMap[teamName.lower()] = (teamId, teamName, "nfl")
             else:
                 teamSet.add(teamId)
             cursor += 1
@@ -35,5 +67,7 @@ def getTeams():
         raise
 
 # Fetch teams on startup
-TeamsByName = getTeams()
+allTeams = getCfbTeams()
+allTeams.update(getNflTeams())
+TeamsByName = allTeams
 fuzzymatch = lambda text, num : difflib.get_close_matches(text.lower(), TeamsByName.keys(), num, 0.2)
